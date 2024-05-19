4c3604c32614ca47b1d6313d8af5e99df067d109
rajvanshyr <rajvanshyr@utexas.edu>
2024-05-19 14:38:52 -0400

    Your hella good commit message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-	return "<h1 style='color:green'>Hella World!</h1>"
+	return "<h1 style='color:green'>Hella Yeah World!</h1>"
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
