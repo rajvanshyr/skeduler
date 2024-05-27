0f43bcebf1677492e69e555a89f586879d32ab7c
rajvanshyr <rajvanshyr@utexas.edu>
2024-05-27 14:03:03 -0400

    new branch deployment

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import uuid
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy import text
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://u21vm5h75knqle:p449e4facb4556152409922cbd0a26ca356b593e7336dd9e60bf72de4b051e1eb@c97r84s7psuajm.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d1sqbtf1qlslff'
 #connect to postgres db 
